bd2461caf7af2f94e4f85da4855863592907d562
Seyone Gunasingham <s.g.seyone@proton.me>
2026-05-24 13:50:47 +0530

    fix(proxy): add production domain to hostname check

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,7 +6,7 @@ import {genericOAuth, keycloak} from "better-auth/plugins";
 import {db} from "@/db/db";
 import {eq} from "drizzle-orm";
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = true;
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
